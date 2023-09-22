4e8504754683696ce8ad5b2f06ae85aeca9e3bf5
yohan <noyohanx@naver.com>
2023-09-22 15:54:52 +0900

    :sparkles: :: nat 등 vpc 관련 작업 완료

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -57,5 +57,96 @@ resource "aws_internet_gateway" "igw" {
     )
 }
 
+resource "aws_eip" "nat_eip" {
+    for_each = { for subnet in var.single_nat_gateway ? [try(local.public_subnets[0], null)] : local.public_subnets : format("public_subnet_%s", subnet.availability_zone) => subnet if length(var.public_subnet_cidrs) > 0 && var.enable_nat_gateway }
+
+    domain      = "vpc"
+    depends_on = [aws_internet_gateway.igw]
+
+    tags = merge(
+        {
+        Name = var.single_nat_gateway ? "${var.namespace}-nat-eip" : format("%s-nat-eip-%s", var.namespace, split("-", each.value.availability_zone)[2])
+        },
+        local.additional_tags
+    )
+}
 
 
+resource "aws_nat_gateway" "nat" {
+    for_each = { for subnet in var.single_nat_gateway ? [try(local.public_subnets[0], null)] : local.public_subnets : format("public_subnet_%s", subnet.availability_zone) => subnet if length(var.public_subnet_cidrs) > 0 && var.enable_nat_gateway }
+
+    allocation_id = aws_eip.nat_eip[each.key].id
+    subnet_id     = aws_subnet.public_subnet[each.key].id
+    depends_on    = [aws_internet_gateway.igw]
+
+    tags = merge(
+    {
+        Name = var.single_nat_gateway ? "${var.namespace}-nat" : format("%s-nat-%s", var.namespace, split("-", each.value.availability_zone)[2])
+    },
+    local.additional_tags
+    )
+}
+
+resource "aws_route_table" "public" {
+    count = length(var.public_subnet_cidrs) > 0 ? 1 : 0
+
+    vpc_id = aws_vpc.vpc.id
+
+    tags = merge(
+    {
+        Name = "${var.namespace}-public-rtb"
+    },
+    local.additional_tags
+    )
+}
+
+resource "aws_route" "public_internet_gateway" {
+    count = length(var.public_subnet_cidrs) > 0 ? 1 : 0
+
+    route_table_id         = aws_route_table.public[0].id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id             = aws_internet_gateway.igw[0].id
+
+    timeouts {
+        create = "5m"
+    }
+}
+
+resource "aws_route_table_association" "public" {
+    for_each = { for subnet in local.public_subnets : format("public_subnet_%s", subnet.availability_zone) => subnet if length(var.public_subnet_cidrs) > 0 }
+
+    subnet_id      = aws_subnet.public_subnet[each.key].id
+    route_table_id = aws_route_table.public[0].id
+}
+
+resource "aws_route_table" "private" {
+    for_each = { for subnet in local.private_subnets : format("private_subnet_%s", subnet.availability_zone) => subnet if length(var.public_subnet_cidrs) > 0 && var.enable_nat_gateway && length(var.private_subnet_cidrs) > 0 }
+
+    vpc_id = aws_vpc.vpc.id
+
+    tags = merge(
+    {
+        Name = format("%s-private-rtb-%s", var.namespace, split("-", each.value.availability_zone)[2])
+    },
+    local.additional_tags
+    )
+}
+
+resource "aws_route" "private_nat_gateway" {
+    for_each = { for subnet in local.private_subnets : format("private_subnet_%s", subnet.availability_zone) => subnet if length(var.public_subnet_cidrs) > 0 && var.enable_nat_gateway && length(var.private_subnet_cidrs) > 0 }
+
+    route_table_id         = aws_route_table.private[each.key].id
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id         = var.single_nat_gateway ? aws_nat_gateway.nat[format("public_subnet_%s", local.public_subnets[0].availability_zone)].id : aws_nat_gateway.nat[format("public_subnet_%s", each.value.availability_zone)].id
+
+    timeouts {
+        create = "5m"
+    }
+}
+
+resource "aws_route_table_association" "private" {
+    for_each = { for subnet in local.private_subnets : format("private_subnet_%s", subnet.availability_zone) => subnet if length(var.public_subnet_cidrs) > 0 && var.enable_nat_gateway && length(var.private_subnet_cidrs) > 0 }
+
+    subnet_id      = aws_subnet.private_subnet[each.key].id
+    route_table_id = aws_route_table.private[each.key].id
+}
