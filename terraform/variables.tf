f7b526348e8b687dbc26ea475b41e4e3b0d0d110
yohan <noyohanx@naver.com>
2023-09-22 09:39:11 +0900

    :sparkles: :: variables.tf 작성

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+    default     = "ap-northeast-2"
+    type        = string
+    description = "AWS Region"
+}
+
+variable "namespace" {
+    default     = "test-eks" 
+    type        = string
+    description = "Project namespace"
+}
+
+variable "environment" {
+    default     = "dev"
+    type        = string
+    description = "deployment evironment"
+}
+
+variable "purpose" {
+    type        = string
+    description = "purpose of the resource"
+}
