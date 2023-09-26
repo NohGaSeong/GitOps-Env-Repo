19e6ff44a32e8b845d1ca1bb5cf4ba61a4e167af
yohan <noyohanx@naver.com>
2023-09-26 20:36:39 +0900

    :bug: :: 사소한 에러 수정

--- terraform/sg-group.tf
+++ terraform/sg-group.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "bastion-sg" {
         from_port   = 0
         to_port     = 0
         protocol    = "-1"
-        cidr_blocks = "[0.0.0.00]" 
+        cidr_blocks = ["0.0.0.0/0"]
     }
     tags = merge(
         {
